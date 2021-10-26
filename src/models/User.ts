0ad8729a309e583366d602f14c87f37cfb547645
Josue <josue.silva@erzinger.com.br>
2021-10-26 20:17:35 -0300

    created user db schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,50 @@
-import { } from 'mongoose';
+import { model, Schema } from 'mongoose';
+
+interface User {
+    name: string
+    biography: string
+    email: string
+    password: string
+    following: string[]
+    followers: string[]
+    movies: string[]
+    lastAccess: number
+}
+
+export default model<User>(
+    'Users',
+    new Schema<User>({
+        name: {
+            type: String,
+            required: true,
+        },
+        biography: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        following: {
+            type: [String],
+            required: true,
+        },
+        followers: {
+            type: [String],
+            required: true,
+        },
+        movies: {
+            type: [String],
+            required: true,
+        },
+        lastAccess: {
+            type: Number,
+            required: false,
+        },
+    })
+);
