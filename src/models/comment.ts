9442e9d061b4931a2ae29949e310b536126999f4
Josue <josue.silva@erzinger.com.br>
2021-10-26 22:43:24 -0300

    added comments model

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -1,12 +1,33 @@
 import { model, Schema } from 'mongoose';
 
 interface Comment {
+    user: Schema.Types.ObjectId
+    movie: Schema.Types.ObjectId
+    comment: string
+    score: number
 }
 
 export default model<Comment>(
     'Comments',
     new Schema<Comment>({
-
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'Users',
+            required: true,
+        },
+        movie: {
+            type: Schema.Types.ObjectId,
+            ref: 'Movies',
+            required: true,
+        },
+        comment: {
+            type: String,
+            required: true,
+        },
+        score: {
+            type: Number,
+            required: true,
+        },
     })
 );
 
