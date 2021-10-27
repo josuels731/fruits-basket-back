bf29f8e2db0ecdca1e87942726d8a5a279c05161
Josue <josue.silva@erzinger.com.br>
2021-10-26 22:29:57 -0300

    movies routes finished

--- src/routes/movies.d.ts
+++ src/routes/movies.d.ts
@@ -1,10 +1,27 @@
 import { Request, Response } from 'express'
+import { Movie } from '../models/movie'
 
-interface RequestPostNew extends Request {
-    body: {
+interface RequestGet extends Request {
+}
+interface ResponseGet {
+    error?: string,
+    movies?: Movie[]
+}
 
+interface RequestGetId extends Request {
+    params: {
+        id: string
     }
 }
+interface ResponseGetId {
+    error?: string,
+    movie?: Movie
+}
+
+interface RequestPostNew extends Request {
+    body: Movie
+}
 interface ResponsePostNew {
-    error?: string
+    error?: string,
+    name?: string
 }
--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,10 +1,61 @@
 // Libraries
-import { Router, } from "express";
-import { } from './movies.d'
+import { Router, Response } from "express";
+import { RequestGet, RequestGetId, RequestPostNew, ResponseGet, ResponseGetId, ResponsePostNew } from './movies.d'
 
 // Database Models
+import Movie from '../models/movie';
 
 // Code
 const movies = Router();
+
+movies.get('/', async (request: RequestGet, response: Response<ResponseGet>) => {
+    try {
+        const movies = await Movie.find();
+        console.log(request.params);
+
+        if (movies)
+            response.status(200).send({ movies: movies });
+
+    } catch (e) {
+        if (e instanceof Error)
+            response.status(500).send({ error: e.message })
+        else
+            response.status(500).send({ error: 'Internal Error' })
+
+    }
+});
+
+movies.get('/:id', async (request: RequestGetId, response: Response<ResponseGetId>) => {
+    try {
+        const movie = await Movie.findById(request.params.id)
+
+        if (movie)
+            response.status(200).send({ movie: movie });
+
+    } catch (e) {
+        if (e instanceof Error)
+            response.status(500).send({ error: e.message })
+        else
+            response.status(500).send({ error: 'Internal Error' })
+
+    }
+});
+
+
+movies.post('/new', async (request: RequestPostNew, response: Response<ResponsePostNew>) => {
+    const newMovie = new Movie(request.body);
+
+    try {
+        const save = await newMovie.save();
+
+        response.status(201).send({ name: save.name })
+    } catch (e) {
+        if (e instanceof Error)
+            response.status(500).send({ error: e.message })
+        else
+            response.status(500).send({ error: 'Internal Error' })
+
+    }
+});
 
 export { movies };
